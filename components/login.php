c3bb8dbb8d18f00e116651df470a35ea4f3cb0c0
Cabrera22A3 <solesourc3@gmail.com>
2024-08-23 15:46:47 +0800

    1st commit

---
+++ components/login.php
@@ -0,0 +1,19 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+    <head>
+        <title></title>
+        <link rel="stylesheet" href="../styles/login.css">
+        <link rel="stylesheet" href="../styles/fonts.css">
+    </head>
+    <body>
+        <div class="login-bg">
+            <div class="login-username">
+                <h5>Username:</h5>
+                <input name="q" placeholder="Enter your username">
+            </div>
+        </div>
+    </body>
+</html>
